01eafa25c7345f894dac2bea5274737a76e4ede9
vsenapathi <vathsalya.senapathi@gmail.com>
2022-07-19 12:51:33 -0500

    Added axios + dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function goSearch(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {

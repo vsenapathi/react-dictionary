692d14018a3f2cc277b0074617d962c795b896b1
vsenapathi <vathsalya.senapathi@gmail.com>
2022-07-19 17:05:58 -0500

    emoji reference fix

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="nonreferrer">
-        <span role={img}>🔈</span>
+        🔈
       </a>{" "}
       {props.phonetic.text}
     </div>

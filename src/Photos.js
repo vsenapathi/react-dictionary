65e310b411d4b760312a5dc13dee4af7c145c802
vsenapathi <vathsalya.senapathi@gmail.com>
2022-07-20 23:10:27 -0500

    Styled photos component

--- src/Photos.js
+++ src/Photos.js
@@ -1,3 +1,23 @@
 import React from "react";
 
-export default function Photos(props) {}
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <div className="Photos container text-center">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.tiny} alt="result" />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
